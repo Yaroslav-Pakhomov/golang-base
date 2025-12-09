640f5174c494edc5a0be544762f09cc1658c3231
Пахомов Ярослав Андреевич <Y.Pakhomov@nir.center>
2025-12-09 11:16:16 +0500

    Арифметические действия
    - Задание 1. Сумма чисел;
    
    - Задание 2. Программа “Калькулятор”;
    
    - Задание 3. Программа “Среднее арифметическое”;
    
    - go build;
    
    - доработка .gitignore;

--- main.go
+++ main.go
@@ -1,18 +1,19 @@
 package main
 
-import (
-	"fmt"
-	"golang-base/pkg/askname"
-	"golang-base/pkg/greeting"
-)
+import "golang-base/pkg/mathAct"
 
 func main() {
-	fmt.Println("Hello Go.")
-	fmt.Println("My name is Иван.")
-	fmt.Println("I’m starting to learn Go!")
+	// fmt.Println("Hello Go.")
+	// fmt.Println("My name is Иван.")
+	// fmt.Println("I’m starting to learn Go!")
+	//
+	// // Переменные и конкатенация строк
+	// greeting.Greeting()
+	// // Ввод данных от пользователя
+	// askname.Askname()
 
-	// Переменные и конкатенация строк
-	greeting.Greeting()
-	// Ввод данных от пользователя
-	askname.Askname()
+	// Арифметические действия
+	// mathAct.Summa()
+	// mathAct.Calculator()
+	mathAct.ArithmeticMean()
 }
---
+++ pkg/mathAct/mathAct.go
@@ -0,0 +1,62 @@
+package mathAct
+
+import "fmt"
+
+// Summa - рассчёт суммы трёх чисел
+func Summa() {
+	var a, b, c int
+
+	fmt.Println("Введите первое число:")
+	fmt.Scan(&a)
+
+	fmt.Println("Введите второе число:")
+	fmt.Scan(&b)
+
+	fmt.Println("Введите третье число:")
+	fmt.Scan(&c)
+
+	sum := a + b + c
+	fmt.Println("Сумма чисел:", sum)
+}
+
+// Calculator - выполняет действия: +, -, *, /
+func Calculator() {
+	var a, b int
+	var operation string
+
+	fmt.Println("Введите первое число:")
+	fmt.Scan(&a)
+
+	fmt.Println("Введите второе число:")
+	fmt.Scan(&b)
+
+	fmt.Println("Какую операцию выполнить: +, -, *, /")
+	fmt.Scan(&operation)
+
+	switch operation {
+	case "+":
+		fmt.Println("Сумма чисел:", a+b)
+	case "-":
+		fmt.Println("Разность между первым и вторым:", a-b)
+	case "*":
+		fmt.Println("Произведение чисел:", a*b)
+	case "/":
+		if b != 0 {
+			fmt.Println("Деление первого на второе:", a/b)
+		} else {
+			fmt.Println("Деление на 0")
+		}
+	}
+}
+
+func ArithmeticMean() {
+	var a, b, c, d int
+
+	fmt.Println("Введите четыре числа:")
+	fmt.Scan(&a)
+	fmt.Scan(&b)
+	fmt.Scan(&c)
+	fmt.Scan(&d)
+
+	fmt.Println("Среднее ариыметическое 4-ёх чисел равно:", (a+b+c+d)/4)
+}
