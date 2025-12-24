10c8d3dcf609d767127478ab65faeadadbcca0cc
Пахомов Ярослав Андреевич <Y.Pakhomov@nir.center>
2025-12-24 10:22:41 +0500

    Type Assertion и Type Switch для Структур и Интерфейсов
    - Структура, Создание, Пример;
    
    - Задание 1. Проверка, что shape - это структура Circle (Type Assertion);
    
    - Задание 2. Проверка типа фигуры и вывод соответствующих данных (Type Switch);
    
    - go build;
    
    - Общие доработки;

--- pkg/level_2/structAndMethDeep/structAndMethDeep.go
+++ pkg/level_2/structAndMethDeep/structAndMethDeep.go
@@ -43,8 +43,19 @@ func GetStructDeep() {
 
 	for i, shape := range shapes {
 		fmt.Printf("%d.", i+1)
+		DescribeShape(shape)
 		PrintShapesInfo(shape)
 	}
+
+	// Type Assertion
+	PrintCircleRadius(rectangle)
+	PrintCircleRadius(circle)
+	fmt.Println("")
+
+	// Type Switch
+	DescribeShape(rectangle)
+	DescribeShape(circle)
+	DescribeShape(triangle)
 }
 
 // ===================================================================================
@@ -55,6 +66,40 @@ func PrintShapesInfo(s Shape) {
 	fmt.Printf("  Площадь: %.2f\n", s.Area())
 	fmt.Printf("  Периметр: %.2f\n", s.Perimeter())
 	fmt.Printf("-------------------------------------------\n\n")
+}
+
+// ===================================================================================
+
+// Функция на применение Type Assertion
+
+// PrintCircleRadius - проверка, что shape - это структура Circle
+func PrintCircleRadius(shape Shape) {
+
+	// Type Assertion
+	checkCircle, success := shape.(Circle)
+
+	// fmt.Println(checkCircle, success)
+	if success {
+		fmt.Printf("Радиус круга равен %v\n", checkCircle.radius)
+	} else {
+		fmt.Println("Фигура не является кругом")
+	}
+}
+
+// Функция на применение Type Switch
+
+// DescribeShape - проверка типа фигуры и вывод соответствующих данных
+func DescribeShape(shape Shape) {
+	switch shapeType := shape.(type) {
+	case Rectangle:
+		fmt.Printf("Фигура Прямоугольник. Ширина равна %v , высота %v. \n", shapeType.Width, shapeType.Height)
+	case Circle:
+		fmt.Printf("Фигура Круг. Радиус круга равен %v. \n", shapeType.radius)
+	case Triangle:
+		fmt.Printf("Фигура Треугольник. Сторона \"А\" равна %v, сторона \"В\" равна %v, сторона \"С\" равна %v. \n", shapeType.A, shapeType.B, shapeType.C)
+	default:
+		fmt.Printf("Неизвестная фигура")
+	}
 }
 
 // ===================================================================================
@@ -136,3 +181,74 @@ func (t Triangle) Perimeter() float64 {
 func (t Triangle) IsValid() bool {
 	return t.A+t.B > t.C && t.A+t.C > t.B && t.B+t.C > t.A
 }
+
+// ===================================================================================
+
+// Интерфейс
+// type Speaker interface {
+// 	Speak() string
+// }
+
+// Структура
+// type Human struct {}
+// func (h Human) Speak() string {
+// 	return "Hello"
+// }
+
+// Type assertion — это проверка и извлечение конкретного типа из интерфейса.
+
+// Синтаксис:
+// value, ok := interfaceValue.(ConcreteType)
+
+// 		- value — значение конкретного типа
+// 		- ok — true, если приведение успешно
+// 		- interfaceValue - интерфейс, который реализует структура
+// 		- ConcreteType - структура, проверка на принадлежность
+
+// Пример Type assertion:
+
+// Переменная Структуры "Human", реализующая Интерфейс "Speaker"
+// var s Speaker = Human{}
+
+// Type assertion:
+// 	h, ok := s.(Human)
+// 	if ok {
+// 		fmt.Println("Это Human:", h)
+// 	}
+
+// Опасно:
+// h := s.(Human) // panic, если тип не Human
+
+// ===================================================================================
+
+// Type switch — удобный способ проверить несколько типов интерфейса.
+
+// Синтаксис
+// 	switch v := interfaceValue.(type) {
+// 	case Type1:
+// 		...
+// 	case Type2:
+// 		...
+// 	default:
+// 		...
+// 	}
+
+// Пример Type Switch со структурами:
+
+// func describe(s Speaker) {
+// 	switch v := s.(type) {
+// 	case Human:
+// 		fmt.Println("Human говорит:", v.Speak())
+// 	case Dog:
+// 		fmt.Println("Dog говорит:", v.Speak())
+// 	default:
+// 		fmt.Println("Неизвестный тип")
+// 	}
+// }
+
+// Когда использовать:
+// Ситуация							Решение
+// Проверка одного типа				type assertion
+// Много возможных типов			type switch
+// Бизнес-логика зависит от типа	type switch
+// Полиморфизм без проверок			интерфейсные методы
